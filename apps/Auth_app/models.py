24a1439b41de3d9e6b027d342f77285286cf9bca
YoussifAllam <96921160+YoussifAllam@users.noreply.github.com>
2024-09-25 18:32:23 +0300

    add groups and user_permissions to user model

--- apps/Auth_app/models.py
+++ apps/Auth_app/models.py
@@ -17,6 +17,21 @@ class User(AbstractUser):
     created_Date = models.DateTimeField(auto_now_add=True)
     last_login = models.DateTimeField(auto_now_add=True)
 
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='custom_user_set',  # Change this name to something unique
+        blank=True,
+        help_text='The groups this user belongs to.',
+        verbose_name='groups',
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='custom_user_permissions_set',  # Change this name to something unique
+        blank=True,
+        help_text='Specific permissions for this user.',
+        verbose_name='user permissions',
+    )
+
     def __str__(self):
         return self.username
 
