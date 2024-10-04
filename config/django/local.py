881895a21eb6b40f966392cac36ee479dd7bdfff
YoussifAllam <96921160+YoussifAllam@users.noreply.github.com>
2024-10-04 17:17:34 +0300

    change mount path in socker-compose

--- config/django/local.py
+++ config/django/local.py
@@ -19,7 +19,7 @@ MIDDLEWARE += [  # noqa
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME':  join(BASE_DIR, 'db.sqlite3'),
+        'NAME':  join(UPPER_DIR, 'db.sqlite3'),
     }
 }
 
